3acb97759f28806ebdbe4d34425237c34f641389
v_liupeng <system_lp@126.com>
2017-08-17 18:25:36 +0800

    登录页

--- hello-world/src/index.js
+++ hello-world/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import {render} from 'react-dom';
-import {Router, Route, browserHistory} from 'react-router';
+import {Router, Route, browserHistory, IndexRoute} from 'react-router';
 import App from './App';
 import Home from './home/Home';
 import _Button from './button/_Button';
@@ -10,20 +10,22 @@ import _Affix from './affix/_Affix';
 import _Cascader from './cascader/_Cascader';
 import Hello from './Hello';
 import ECharts from './echarts/ECharts';
+import WrappedNormalLoginForm from './login/Login';
 // import registerServiceWorker from './registerServiceWorker';
 
 render((
     <Router history={browserHistory}>
-        <Route component={App}>
-            <Route path="/" component={Home}/>
-            <Route path="/echarts" component={ECharts}/>
-            <Route path="/button" component={_Button}/>
-            <Route path="/icon" component={_Icon}/>
-            <Route path="/grid" component={_Grid}/>
-            <Route path="/affix" component={_Affix}/>
-            <Route path="/cascader" component={_Cascader}/>
-            <Route path="/Hello" component={Hello}/>
+        <Route path="/" component={App}>
+            <IndexRoute component={Home}/>
+            <Route path="echarts" component={ECharts}/>
+            <Route path="button" component={_Button}/>
+            <Route path="icon" component={_Icon}/>
+            <Route path="grid" component={_Grid}/>
+            <Route path="affix" component={_Affix}/>
+            <Route path="cascader" component={_Cascader}/>
+            <Route path="Hello" component={Hello}/>
         </Route>
+        <Route path="/login" component={WrappedNormalLoginForm}/>
     </Router>
 ), document.getElementById('root'));
 // registerServiceWorker();
